8b005ead34a1fddfecfaec70cfcf41fd038bc580
EndureBlackout <jacobellingson@aol.com>
2016-09-06 10:16:22 -0500

    Changed chat color class import

--- src/me/endureblackout/capturepoint/PlayerListener.java
+++ src/me/endureblackout/capturepoint/PlayerListener.java
@@ -14,7 +14,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
 
-import net.md_5.bungee.api.ChatColor;
+import or.bukkit.chatcolor;
 
 public class PlayerListener implements Listener {
 	CapturePointMain plugin;
